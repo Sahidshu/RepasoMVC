6689c47f8990b39f0dc2ddb6d1e146abae3007f0
Notteth <sahidmunoz0@gmail.com>
2025-02-07 15:25:47 -0500

    Repaso MVC

---
+++ src/main/java/espoch/edu/ec/repasomvc/RepasoMVC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.repasomvc;
+
+/**
+ *
+ * @author sahid
+ */
+public class RepasoMVC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
