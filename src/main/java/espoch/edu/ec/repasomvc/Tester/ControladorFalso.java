ba3dc908d320c1c59fd9e35863765381497a1e9a
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2025-02-07 17:59:22 -0500

    Actualizaciones

--- src/main/java/espoch/edu/ec/repasomvc/Tester/ControladorFalso.java
+++ src/main/java/espoch/edu/ec/repasomvc/Tester/ControladorFalso.java
@@ -7,4 +7,5 @@ package espoch.edu.ec.repasomvc.Tester;
  */
 public class ControladorFalso {
     
+    
 }
--- src/main/java/espoch/edu/ec/repasomvc/controlador/Controlador.java
+++ src/main/java/espoch/edu/ec/repasomvc/controlador/Controlador.java
@@ -10,7 +10,39 @@ package espoch.edu.ec.repasomvc.controlador;
  */
 public class Controlador {
     
+  /*  private Principal principal;
     
+
+    public Controlador(Principal principal) {
+        this.principal = principal;
+    }
+    
+    
+    public TesterControler(Principal principal) {
+        this.principal = principal;
+        this.testerControlador =  new TesterControler(principal);
+    }
+    
+    
+ //dirije a cada unom de las funciones
+    public void ahgregarTarea(){
+        //ulizamocs  para errores, como por ejemplo si los compos estan vacios 
+        try{      
+            
+            objTarea.setTitulo(this.principal.getTitle());
+            objTarea.setDescripcion(this.principal.getDescripcion());
+            objTarea.setEstado(this.principal.getEstado()));
+                  
+        if (this.principal != null)
+            testercontroler.prueba(objTarea);
+            
+        } catch (Exception e){
+            
+        }
+        
+    }*/
+    
+
     
     
 }
